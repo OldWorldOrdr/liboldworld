d93549ebd488df72bd30fbca41e654b52ec522dc
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-06-22 22:46:10 -0400

    inital commit

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+#[macro_export]
+macro_rules! printerr {
+    ($($arg:tt)*) => ({
+        eprintln!("\x1B[1;31mError:\x1B[0m {}", format_args!($($arg)*));
+        std::process::exit(1);
+    })
+}
+
+#[macro_export]
+macro_rules! printerr_noexit {
+    ($($arg:tt)*) => ({
+        eprintln!("\x1B[1;31mError:\x1B[0m {}", format_args!($($arg)*));
+    })
+}
+
+#[macro_export]
+macro_rules! printwarn {
+    ($($arg:tt)*) => ({
+        eprintln!("\x1B[1;33mWarning:\x1B[0m {}", format_args!($($arg)*));
+    })
+}
+
+#[macro_export]
+macro_rules! printinfo {
+    ($($arg:tt)*) => ({
+        eprintln!("\x1B[1;34mInfo:\x1B[0m {}", format_args!($($arg)*));
+    })
+}
+
+fn main() {
+    printerr_noexit!("This is a test");
+    printwarn!("This is a test");
+    printinfo!("This is a test");
+    printerr!("This is a test");
+}
